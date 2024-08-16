f01a9fb1bda07d340ff4a0217cc71aff058e53de
Yannick <nzalikyannick01@gmail.com>
2024-08-16 11:29:41 +0200

    Test from the frontend

--- Generator/auto/simple.sh
+++ Generator/auto/simple.sh
@@ -10,33 +10,16 @@ WORKER_DIR="/home/ykoagnenzali/Experimentations/synchronizeExperimentation/"
 
 echo "Script deployment with : $1"
 
+DEPLOY_SCREEN_NAME="deployment"
+
+WORKER_COMMAND="export PATH=\"\$HOME/.local/bin:\$PATH\"; export KUBECONFIG=/home/admin.conf; kubectl get nodes"
+
 # shellcheck disable=SC2087
-ssh "$NODE_SSH_HOST" << 'EOSSH'
-set -e  # Arrête le script en cas d'erreur
+ssh "$NODE_SSH_HOST" << EOF
+  # Arrêt de la screen existante
+  screen -S $DEPLOY_SCREEN_NAME -X quit
 
-# Créer une nouvelle session screen détachée
-screen -dmS my_session bash -c '
-    set -e  # Arrête le script en cas d'erreur
-
-    # Ajouter le répertoire .local/bin au PATH
-    export PATH="$HOME/.local/bin:$PATH"
-
-    echo "le path sur le noeud"
-    echo $PATH
-
-    # Mise à jour Git
-    git pull
-
-    pwd
-
-    # Configuration KUBECONFIG
-    export KUBECONFIG=/home/ykoagnenzali/admin.conf
-
-    # Exécution de commandes kubectl
-    kubectl get nodes > nodes.txt
-    kubectl create -f custom_deployments/teastore-clusterip-1cpu-5giga.yaml > deploy.txt
-    kubectl get pods > pods.txt
-'
-
-# Fin de la session SSH
-EOSSH
+  # Création d'une nouvelle screen et lancement du worker
+  #cd $WORKER_DIR
+  screen -S $DEPLOY_SCREEN_NAME -d -m $WORKER_COMMAND
+EOF
