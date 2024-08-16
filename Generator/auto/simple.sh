89b970037997a7d3f122f6852addf276b3bc103c
Yannick <nzalikyannick01@gmail.com>
2024-08-16 11:38:08 +0200

    Test from the frontend

--- Generator/auto/simple.sh
+++ Generator/auto/simple.sh
@@ -6,13 +6,17 @@ echo "$PATH"
 
 NODE_SSH_HOST=$1
 
-WORKER_DIR="/home/ykoagnenzali/Experimentations/synchronizeExperimentation/"
+#WORKER_DIR="/home/ykoagnenzali/Experimentations/synchronizeExperimentation/"
+# shellcheck disable=SC2088
+WORKER_DIR="~/Experimentations/synchronizeExperimentation/worker"
 
 echo "Script deployment with : $1"
 
 DEPLOY_SCREEN_NAME="deployment"
 
-WORKER_COMMAND="export PATH=\"\$HOME/.local/bin:\$PATH\"; export KUBECONFIG=/home/admin.conf; kubectl get nodes"
+WORKER_COMMAND="java -jar -Xms16g -Xmx32g -Xss256k ./httploadgenerator.jar loadgenerator"
+
+#WORKER_COMMAND="export PATH=\"\$HOME/.local/bin:\$PATH\"; export KUBECONFIG=/home/admin.conf; kubectl get nodes"
 
 # shellcheck disable=SC2087
 ssh "$NODE_SSH_HOST" << EOF
@@ -20,6 +24,6 @@ ssh "$NODE_SSH_HOST" << EOF
   screen -S $DEPLOY_SCREEN_NAME -X quit
 
   # Création d'une nouvelle screen et lancement du worker
-  #cd $WORKER_DIR
+  cd $WORKER_DIR
   screen -S $DEPLOY_SCREEN_NAME -d -m $WORKER_COMMAND
 EOF
