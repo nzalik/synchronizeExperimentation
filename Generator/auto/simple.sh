89e3829143b2748b23f213d63edbae76d3232054
Yannick <nzalikyannick01@gmail.com>
2024-08-16 14:29:59 +0200

    Construction of the okay version

--- Generator/auto/simple.sh
+++ Generator/auto/simple.sh
@@ -2,19 +2,22 @@
 
 NODE_SSH_HOST=$1
 
-# shellcheck disable=SC2034
-DEPLOY_SCREEN_NAME="deployment1"
-
-echo "Script deployment with : $1"
-
 # shellcheck disable=SC2087
 ssh "$NODE_SSH_HOST" << EOF
 
+  echo "Script deployment with : $1"
+  echo "target deployment with : $2"
+
+  WORKER_DIR="/home/ykoagnenzali/Experimentations/synchronizeExperimentation/"
+
+  cd $WORKER_DIR
+  pwd
   export PATH="$HOME/.local/bin:$PATH"
   export KUBECONFIG=/home/ykoagnenzali/admin.conf
-  kubectl
+
   kubectl get nodes
-  kubectl get config
+  kubectl create -f ./custom_deployments/teastore-clusterip-1cpu-5giga.yaml
+
   echo "on vient de finir"
   echo "###############"
 EOF
--- Generator/deploy_group_scaling.sh
+++ Generator/deploy_group_scaling.sh
@@ -2,45 +2,150 @@
 
 NODE_SSH_HOST=$1
 
-WORKER_DIR="/home/ykoagnenzali/Experimentations/synchronizeExperimentation/"
-
 echo "Script deployment with : $1"
 
 # shellcheck disable=SC2087
 ssh "$NODE_SSH_HOST" << EOF
-set -e  # Arrête le script en cas d'erreur
 
-sudo-g5k apt update
+export PATH="$HOME/.local/bin:$PATH"
 
-# Téléchargement de kubectl
-curl -LO "https://dl.k8s.io/release/\$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-chmod +x ./kubectl
-mv ./kubectl /usr/local/bin/kubectl
+target=$2
 
-# Vérification de la version kubectl
-kubectl version --client
-
-# Affichage du PATH pour diagnostic
-echo \$PATH
-
-# Cible et variable de répertoire
-target=\$2
-echo "target=\$target"
-echo "WORKER_DIR=\$WORKER_DIR"
-
-# Mise à jour Git
 git pull
 
-# Navigation dans le répertoire de travail
-cd \$WORKER_DIR || exit
+nb_thread=128
+
+# Obtenir le répertoire parent
+parent_dir=$(dirname $(pwd))
+
+# Obtenir la date actuelle
+date_str=$(date +"%d-%m-%Y")
+
+category="128/group/3nodes/linear"
+
+# Chemin complet du nouveau dossier
+new_folder_path="$parent_dir/nantes/hyperthreading/$category/$date_str"
+
+# Créer le nouveau dossier s'il n'existe pas déjà
+if [ ! -d "$new_folder_path" ]; then
+    mkdir -p "$new_folder_path"
+fi
+
+# Compter le nombre de fichiers dans le répertoire $date_str
+file_count=$(ls -1 "$new_folder_path" | wc -l)
+
+# Créer le sous-répertoire "experimentation" avec le numéro
+exp_folder_path="$new_folder_path/experimentation$((file_count + 1))"
+
+if [ ! -d "$exp_folder_path" ]; then
+    mkdir -p "$exp_folder_path"
+fi
+
+
+wOutput="$exp_folder_path/warmup"
+lOutput="$exp_folder_path/data/load"
+
+if [ ! -d "$wOutput" ]; then
+    mkdir -p "$wOutput"
+fi
+
+if [ ! -d "$lOutput" ]; then
+    mkdir -p "$lOutput"
+fi
+
+#sleep 60
+# Liste des fichiers de charge
+# shellcheck disable=SC2054
+#workload_files=(
+#   "intensity_profile-three-21-06-2024-10min-100.0requests.csv",
+#)
+workload_date=$(date +"%Y-%m-%d")
+#workload_dir="../Load/profiles_$workload_date"
+workload_dir="../Load/profiles_2024-07-31"
+warmup_dir="../warmUp"
+
 pwd
 
-# Configuration KUBECONFIG
+workload_files=($(ls "$workload_dir"/*.csv))
+
+
+warmup="const_linear_80requests_per_sec.csv"
+
+warmupFile="../warmUp/${warmup}"
+
+echo $warmupFile
+
 export KUBECONFIG=/home/ykoagnenzali/admin.conf
 
-# Exécution de commandes kubectl
-kubectl get nodes > nodes.txt
-kubectl create -f custom_deployments/teastore-clusterip-1cpu-5giga.yaml > deploy.txt
-kubectl get pods > pods.txt
+#for file_name in workload_files:
+for file_name in "${workload_files[@]}"; do
 
+echo $file_name
+
+input_string=$file_name
+output_part=$(basename "$input_string" .csv)
+output_part="${output_part#profiles_}"
+echo "$output_part"
+
+echo "##################### Initialisation ##################################################"
+
+# Créer le déploiement Kubernetes
+kubectl create -f ../custom_deployments/teastore-clusterip-1cpu-5giga.yaml
+
+sleep 240
+
+echo "##################### Sleeping before warmup ##################################################"
+
+warm="warmup-$output_part.csv"
+
+#Lancer le générateur de charge HTTP
+java -jar httploadgenerator.jar director -s $target -a "$warmupFile" -l "./teastore_buy.lua" -o "$warm" -t $nb_thread
+
+#sleep 180
+
+#echo "##################### Sleeping before autoscaler ##################################################"
+
+
+#kubectl create -f "../autoscalers/autoscaler-nodb-noregistry-5-max-replicas-70percent.yaml"
+
+
+echo "##################### Sleeping before load ##################################################"
+
+sleep 180
+
+#result="$output_part.csv"
+result="output-$output_part.csv"
+
+#res="$output_part.csv"
+
+pip3 install kubernetes
+
+java -jar httploadgenerator.jar director -s $target -a "$file_name" -l "./teastore_buy.lua" -o $result -t $nb_thread  &
+
+java_pid=$!
+
+# Lancer le script Python après 300 secondes
+(sleep 250; python3 ../pvc_debug/scaler-script.py) &
+
+wait $java_pid
+
+echo "#########################Load Injection finished######################################"
+
+sleep 60
+
+#moveRepo="../Load/intensity_profiles_2024-07-14/"
+
+python3 ../Fetcher/PostFetcher.py $result $workload_dir $exp_folder_path
+#python3 ../Fetcher/PostFetcher.py $warm $warmup_dir $exp_folder_path
+
+sleep 60
+
+#mv ../Load/intensity_profiles_2024-07-14/$result $lOutput
+mv "$workload_dir/$result" $lOutput
+
+kubectl delete pods,deployments,services -l app=teastore
+
+sleep 240
+
+done
 EOF
