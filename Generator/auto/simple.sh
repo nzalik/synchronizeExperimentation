0d2bebd09337432816ce5b64c7c6b7f41d9207cd
Yannick <nzalikyannick01@gmail.com>
2024-08-16 14:34:50 +0200

    Construction of the okay version

--- Generator/auto/simple.sh
+++ Generator/auto/simple.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
 NODE_SSH_HOST=$1
+# shellcheck disable=SC2088
+WORKER_DIR="~/Experimentations/synchronizeExperimentation"
 
 # shellcheck disable=SC2087
 ssh "$NODE_SSH_HOST" << EOF
 
-  echo "Script deployment with : $1"
-  echo "target deployment with : $2"
-
-  WORKER_DIR="/home/ykoagnenzali/Experimentations/synchronizeExperimentation/"
-
   cd $WORKER_DIR
+
   pwd
   export PATH="$HOME/.local/bin:$PATH"
   export KUBECONFIG=/home/ykoagnenzali/admin.conf
