75528fd0302c489901eef54b6297fbbefede6f6c
Yannick <nzalikyannick01@gmail.com>
2024-08-17 11:09:12 +0200

    Construction of the okay version 3

---
+++ Generator/auto/deploy_original.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+echo "target $2"
+echo "deployment $1"
+
+NODE_SSH_HOST=$1
+
+target=$2
+
+nb_thread=128
+
+# Obtenir le répertoire parent
+parent_dir=$(dirname $(pwd))
+
+# Obtenir la date actuelle
+date_str=$(date +"%d-%m-%Y")
+
+category="128/group/3nodes/linear"
+
+# Chemin complet du nouveau dossier
+new_folder_path="$parent_dir/nantes/hyperthreading/$category/$date_str"
+
+# Créer le nouveau dossier s'il n'existe pas déjà
+if [ ! -d "$new_folder_path" ]; then
+    mkdir -p "$new_folder_path"
+fi
+
+# Compter le nombre de fichiers dans le répertoire $date_str
+file_count=$(ls -1 "$new_folder_path" | wc -l)
+
+# Créer le sous-répertoire "experimentation" avec le numéro
+exp_folder_path="$new_folder_path/experimentation$((file_count + 1))"
+
+if [ ! -d "$exp_folder_path" ]; then
+    mkdir -p "$exp_folder_path"
+fi
+
+
+wOutput="$exp_folder_path/warmup"
+lOutput="$exp_folder_path/data/load"
+
+if [ ! -d "$wOutput" ]; then
+    mkdir -p "$wOutput"
+fi
+
+if [ ! -d "$lOutput" ]; then
+    mkdir -p "$lOutput"
+fi
+
+# shellcheck disable=SC2088
+WORKER_DIR="~/Experimentations/synchronizeExperimentation"
+
+workload_date=$(date +"%Y-%m-%d")
+#workload_dir="../Load/profiles_$workload_date"
+workload_dir="../Load/profiles_2024-07-31"
+
+warmup_dir="./warmUp"
+
+workload_files=($(ls "$workload_dir"/*.csv))
+
+warmup="const_linear_80requests_per_sec.csv"
+
+warmupFile="../warmUp/${warmup}"
+
+# shellcheck disable=SC2087
+ssh "$NODE_SSH_HOST" << EOF
+
+  cd $WORKER_DIR
+
+  pwd
+  ls ./
+
+  export PATH="$HOME/.local/bin:$PATH"
+  export KUBECONFIG=/home/ykoagnenzali/admin.conf
+  echo workload_files
+
+EOF
---
+++ Generator/sumitJobScreenLoad.py
@@ -0,0 +1,68 @@
+import json
+import os
+import requests
+from time import sleep
+import subprocess
+
+#user = input(f"Grid'5000 username (default is {os.getlogin()}): ") or os.getlogin()
+user = "ykoagnenzali"
+#password = "GPuVQr2G!Rw2YM9"
+password = ""
+
+admin = "root"
+#password = ""
+#password = input("Grid'5000 password (leave blank on frontends): ")
+g5k_auth = (user, password) if password else None
+
+site_id = "nantes"
+cluster = "econome"
+
+api_job_url = f"https://api.grid5000.fr/stable/sites/{site_id}/jobs"
+
+payload = {
+    "resources": "nodes=2,walltime=0:05",
+    "command": "sleep infinity",
+    "stdout": "api-test-stdout2",
+    "properties": f"cluster='{cluster}'",
+    "name": "load_constant1"
+}
+job = requests.post(api_job_url, data=payload, auth=g5k_auth).json()
+job_id = job["uid"]
+
+sleep(20)
+state = requests.get(api_job_url + f"/{job_id}", auth=g5k_auth).json()
+
+# if state != "terminated":
+#     # Deleting the job, because it takes too much time.
+#     requests.delete(api_job_url+f"/{job_id}", auth=g5k_auth)
+#     print("Job deleted.")
+
+servers = state["assigned_nodes"]
+worker_script_path = "../worker/scripts/worker.sh"
+#deployment_script_path = "../Generator/deploy_group_scaling.sh"
+deployment_script_path = "./auto/deploy_original.sh"
+print("the servers are")
+print(servers)
+
+# Define server with f-string (safe for variable substitution)
+server1 = f"{user}@{servers[0]}"
+server2 = f"{user}@{servers[1]}"
+
+# Execute the script on server1 using SSH (more secure)
+
+worker = subprocess.run([worker_script_path, server1], capture_output=True, text=True)
+
+print(worker.stdout)
+#print("en cas deerrer")
+#print(worker.stderr)
+
+# Deployment here
+
+deployment = subprocess.run([deployment_script_path, server2, servers[0]], capture_output=True, text=True)
+
+print("#########################")
+print(deployment.stdout)
+print("en cas dereru")
+print(deployment.stderr)
+# Execute the deployment script on server2
+#subprocess.run(["ssh", servers[1], "bash ./deployment.sh"], check=True)
