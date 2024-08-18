03387e2112a1eae5691f0fb88cdf221ed176ef8e
Yannick <nzalikyannick01@gmail.com>
2024-08-18 22:36:34 +0200

    obtain 4 webui and 4 persistence and 2 auth

--- pvc_debug/scaler-script.py
+++ pvc_debug/scaler-script.py
@@ -35,8 +35,9 @@ def update_replicas(namespace, deployments):
 if __name__ == "__main__":
     # Exemple d'utilisation
     deployments = {
-        "teastore-webui": 6,
-        "teastore-persistence": 2,
+        "teastore-webui": 4,
+        "teastore-persistence": 4,
+        "teastore-auth": 2,
 
     }
 
