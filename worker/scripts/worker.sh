f38671dc2e460e76349dfd5b5a431c20f569f764
Yannick <nzalikyannick01@gmail.com>
2024-08-30 17:41:52 +0200

    Go back on nantes

--- worker/scripts/worker.sh
+++ worker/scripts/worker.sh
@@ -13,8 +13,8 @@ WORKER_SCREEN_NAME="worker"
 
 # Chemin complet du dossier contenant le worker
 # shellcheck disable=SC2088
-#WORKER_DIR="~/Experimentations/synchronizeExperimentation/worker"
-WORKER_DIR="~/synchronizeExperimentation/worker"
+WORKER_DIR="~/Experimentations/synchronizeExperimentation/worker"
+#WORKER_DIR="~/synchronizeExperimentation/worker"
 
 # Commande pour lancer le worker
 WORKER_COMMAND="java -jar -Xms16g -Xmx32g -Xss256k ./httploadgenerator.jar loadgenerator"
