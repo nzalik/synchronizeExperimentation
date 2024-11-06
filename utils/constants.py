40a2f0e9c0e84baa5c1b6103ee4b66c21a82aad1
yannick.nzali <yannick.nzali-koagne@univ-grenoble-alpes.fr>
2024-11-07 00:56:27 +0100

    Test data fetching

--- utils/constants.py
+++ utils/constants.py
@@ -1,7 +1,6 @@
 import configparser
 import json
 import os
-from datetime import datetime
 import requests
 
 
@@ -19,7 +18,7 @@ colors_table = [
     "#3357FF"  # Bleu Foncé
 ]
 
-def get_color_for_serviceInit(service_name):
+def get_color_for_service_init(service_name):
     service_name = service_name.lower()
 
     # Switch case avec 7 cas différents
@@ -93,13 +92,6 @@ def open_file(file_name):
         return json_data
 
 
-def smooth(values, w_size=5):
-    new_values = []
-    for i in range(len(values)):
-        window = values[max(0, i - (w_size - 1) // 2):min(i + w_size // 2 + 1, len(values))]
-        new_values.append(sum(window) / len(window))
-    return new_values
-
 def read_parameters_from_json(file_path):
     with open(file_path, 'r') as file:
         parameters = json.load(file)
@@ -127,61 +119,61 @@ def sort_legend(legend_objects, legend_labels):
 
     return legend_objects_sorted, legend_labels_sorted
 
-def plot_metrics(data, ax):
-    global timestamps
-    results = data['data']['result']
-
-    for result in results:
-        metric = result['metric']
-        source_workload = metric.get('source_workload', 'unknown')
-        destination = metric.get('destination_workload', elt)
-
-        source_workload_txt = source_workload.split('-')[1] if '-' in source_workload else source_workload
-        destination_txt = destination.split('-')[1]
-
-        timestamps = np.arange(0, plot_limit)
-        valuesInit = [0 if point[1] == "NaN" else float(point[1]) for point in
-                      result['values'][:plot_limit]] + [0] * (
-                             plot_limit - min(plot_limit, len(
-                         result['values'])))  # Remplacer NaN par 0 et compléter avec des 0 si nécessaire
-
-        multiplier_note = ''
-        if harmonization and metric_to_plot == "latency":
-            if destination != 'teastore-webui' and metric_to_plot == "latency":
-                valuesInit = [x * 5 for x in valuesInit]  # Multiply values by a factor for non-webui destinations
-                multiplier_note = ' (x5)'
-            else:
-                multiplier_note = ''
-
-
-        values = smooth(valuesInit)
-        heures = [datetime.fromtimestamp(ts).strftime('%M') for ts in timestamps]
-
-        color = get_color_for_serviceInit(source_workload)
-        ax.plot(timestamps, values, color=color,
-                label=f'{source_workload_txt + str(position + 1)} → {destination_txt + str(position + 1)}{multiplier_note}',
-                linestyle=line_styles[position])
-
-    start_time = min(timestamps)
-    end_time = max(timestamps)
-
-    ticks = np.arange(start_time, end_time + 1, plot_window)
-    ticks_seconds = [((ts - start_time) // plot_window) * plot_window for ts in ticks]
-    ax.set_xticks(ticks)
-    ax.set_xticklabels(ticks_seconds)
-
-    ax.set_xlabel('Time (seconds)')
-    ax.set_ylabel('rps (requests per second)')
-    ax.set_title(f'Request volume')
-    #ax.set_title(f'Requests volume send to {elt}')
-    if metric_to_plot=="latency":
-        ax.set_ylabel('Latency (ms)')
-        ax.set_title(f'Request duration')
-        #ax.set_title(f'Request duration to {elt}')
-
-    ax.set_ylim(0, max_value)
-    ax.legend(loc='upper left', frameon=False)
-    return ticks
+# def plot_metrics(data, ax):
+#     global timestamps
+#     results = data['data']['result']
+#
+#     for result in results:
+#         metric = result['metric']
+#         source_workload = metric.get('source_workload', 'unknown')
+#         destination = metric.get('destination_workload', elt)
+#
+#         source_workload_txt = source_workload.split('-')[1] if '-' in source_workload else source_workload
+#         destination_txt = destination.split('-')[1]
+#
+#         timestamps = np.arange(0, plot_limit)
+#         valuesInit = [0 if point[1] == "NaN" else float(point[1]) for point in
+#                       result['values'][:plot_limit]] + [0] * (
+#                              plot_limit - min(plot_limit, len(
+#                          result['values'])))  # Remplacer NaN par 0 et compléter avec des 0 si nécessaire
+#
+#         multiplier_note = ''
+#         if harmonization and metric_to_plot == "latency":
+#             if destination != 'teastore-webui' and metric_to_plot == "latency":
+#                 valuesInit = [x * 5 for x in valuesInit]  # Multiply values by a factor for non-webui destinations
+#                 multiplier_note = ' (x5)'
+#             else:
+#                 multiplier_note = ''
+#
+#
+#         values = smooth(valuesInit)
+#         heures = [datetime.fromtimestamp(ts).strftime('%M') for ts in timestamps]
+#
+#         color = get_color_for_serviceInit(source_workload)
+#         ax.plot(timestamps, values, color=color,
+#                 label=f'{source_workload_txt + str(position + 1)} → {destination_txt + str(position + 1)}{multiplier_note}',
+#                 linestyle=line_styles[position])
+#
+#     start_time = min(timestamps)
+#     end_time = max(timestamps)
+#
+#     ticks = np.arange(start_time, end_time + 1, plot_window)
+#     ticks_seconds = [((ts - start_time) // plot_window) * plot_window for ts in ticks]
+#     ax.set_xticks(ticks)
+#     ax.set_xticklabels(ticks_seconds)
+#
+#     ax.set_xlabel('Time (seconds)')
+#     ax.set_ylabel('rps (requests per second)')
+#     ax.set_title(f'Request volume')
+#     #ax.set_title(f'Requests volume send to {elt}')
+#     if metric_to_plot=="latency":
+#         ax.set_ylabel('Latency (ms)')
+#         ax.set_title(f'Request duration')
+#         #ax.set_title(f'Request duration to {elt}')
+#
+#     ax.set_ylim(0, max_value)
+#     ax.legend(loc='upper left', frameon=False)
+#     return ticks
 
 
 ###################################################Fetcher#############################
@@ -200,7 +192,7 @@ def path_to_save(init_path):
     output_path = f"{init_path}"
     if os.path.exists(init_path):
         # Construire le nouveau nom de répertoire
-        new_dir_name = f"data_{datetime.now().strftime('%H')}"
+        #new_dir_name = f"data_{datetime.now().strftime('%H')}"
         #output_path = f"{init_path}/experimentation-{string_argument}"
         output_path = f"{init_path}"
 
@@ -285,9 +277,9 @@ def init_metric_metadata(metric, pod_name, prom_url):
     elif res != None and res['data']:
         metadata = res['data'][metric][0]
 
-        if (metadata['type'] == "gauge"):
+        if metadata['type'] == "gauge":
             return f"{metric}{{namespace=\"default\",pod=\"{pod_name}\", container=\"{root_container_name}\" }}"
-        elif (metadata['type'] == "counter"):
+        elif metadata['type'] == "counter":
             return f"irate({metric}{{namespace=\"default\",pod=\"{pod_name}\", container=\"{root_container_name}\"}}[{cpu_step}])"
         else:
             return f"{metric}{{namespace=\"default\"}}"
