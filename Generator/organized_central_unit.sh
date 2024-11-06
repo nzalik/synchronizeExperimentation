0d26d460b0ed883ec04cce1893bf4fed15bb6c0c
yannick.nzali <yannick.nzali-koagne@univ-grenoble-alpes.fr>
2024-11-06 12:53:07 +0100

    Cleaning some unuseful files

--- Generator/organized_central_unit.sh
+++ Generator/organized_central_unit.sh
@@ -2,34 +2,42 @@
 
 export PATH="$HOME/.local/bin:$PATH"
 
-# Obtenir le répertoire parent
+# The parent dir of the current file that will be used to create all following folders
+# in which results will be stored
 parent_dir=$(dirname $(pwd))
 
-# Obtenir la date actuelle
+# The date is used to version the experiemntations and to know which experimentation
+# has been done on which day
 date_str=$(date +"%d-%m-%Y")
 
+
+# The relative path for storing experiments data
 category="128/linear/3nodes/linear"
+
+# This is to give an indication to the script from where the script is executed
+# From the home environment or from the Grid
 
 #prefix_folder="/home/erods-chouette/Documents"
 prefix_folder="/home/ykoagnenzali"
 
-# Chemin complet du nouveau dossier
-new_folder_path1="$parent_dir/locust/gab/nantes/hyperthreading/$category/$date_str"
-new_folder_path_backup="$parent_dir/locust/gab/nantes/hyperthreading/$category/$date_str/backup"
+# Complete relative path for data storage
+new_folder_base="$parent_dir/locust/gab/nantes/hyperthreading/$category/$date_str"
+new_folder_path1="$new_folder_base"
+new_folder_path_backup="$new_folder_base/backup"
 
+#Those paths are given to a indicate to the script that fecth data, where the metrics for istio are
+# created and also the 
 istio_path="../istio_metrics.json"
 metric_path="../teastore_grenoble.json"
 
-workload_date=$(date +"%Y-%m-%d")
-#workload_dir="../Load/profiles_$workload_date"
-workload_dir="../Load/profiles_2024-07-31"
-
+# This is the adress of the server where your applciation is deployed plus the 
+# frontend page you want to access
 host="http://econome-22.nantes.grid5000.fr:30080/tools.descartes.teastore.webui"
 
-workload_files=($(ls "$workload_dir"/*.csv))
-
+# The kubernetes credentials to used for enterinf the cluster
 export KUBECONFIG=~/admin_collect-data.conf
 
+# This script deployed every necessary configuration for istio mesh
 /bin/bash "$parent_dir/mesh/istio.sh"
 
 kubectl create secret docker-registry docker-registry-secret --docker-server=https://gricad-registry.univ-grenoble-alpes.fr --docker-username=chouette --docker-password=esVsrrxsLA9sJ_nzPurJ
@@ -37,7 +45,7 @@ kubectl create secret docker-registry docker-registry-secret --docker-server=htt
   #number=$((number + 1))
   #new_folder_path="${new_folder_path1}/${number}"
   for file_name in ../Load/load3/*.csv; do
-    for i in {1..1}; do
+    for i in {1..2}; do
       root_file_name=$(basename "$file_name" .csv)
 
       # Compter le nombre de fichiers dans le répertoire $date_str
@@ -59,7 +67,8 @@ kubectl create secret docker-registry docker-registry-secret --docker-server=htt
       kubectl create -f ../custom_deployments/gricard-teastore.yaml
       #kubectl create -f ../custom_deployments/teastore-clusterip-1cpu-5giga.yaml
 
-      sleep 240
+      sleep 240 # This wai time is necessary because the application after being deployed, need some time to
+                # be ready to process requests
 
       echo "##################### Sleeping befor240e warmup ##################################################"
 
