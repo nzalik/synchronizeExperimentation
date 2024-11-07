c787b2f54c5e5503dbc5ebc954aaec6342fbbf60
yann <nzalikyannick01@gmail.com>
2024-11-07 11:24:34 +0100

    Retry on the grid for collecting data with credentials

--- Generator/organized_central_unit.sh
+++ Generator/organized_central_unit.sh
@@ -60,7 +60,7 @@ kubectl create secret docker-registry docker-registry-secret --docker-server=htt
   #number=$((number + 1))
   #new_folder_path="${new_folder_path1}/${number}"
   for file_name in $prefix_folder/Load/load1/*.csv; do
-    for i in {1..3}; do
+    for i in {1..1}; do
       root_file_name=$(basename "$file_name" .csv)
 
       # Compter le nombre de fichiers dans le répertoire $date_str
